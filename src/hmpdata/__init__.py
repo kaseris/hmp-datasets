008b6536c6665539c9b8d8c48ce12efe18b43c14
Michail Kaseris <mich.kaseris@gmail.com>
2024-03-05 14:56:05 +0200

    Some more utility functions

--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -4,5 +4,6 @@ from hmpdata.human36m._h36m import skeleton_h36m as Human36MSkeleton
 from hmpdata.human36m.loader import prepare_next_batch_impl as prepare_next_batch
 from hmpdata.misc.mocap_dataset import MocapDataset
 from hmpdata.human36m._dlow.euler import get_batch as DLowEulerDataLoader
+from hmpdata.human36m._dlow.utils import define_actions
 
 Human36MDataset = MocapDataset
