3ee9d065f88714d5a2bdc7df77a9bbc0bde769cd
Michail Kaseris <mich.kaseris@gmail.com>
2024-03-05 13:56:23 +0200

    Interface with the DLow loader

--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -3,5 +3,6 @@ __version__ = '1.0.0'
 from hmpdata.human36m._h36m import skeleton_h36m as Human36MSkeleton
 from hmpdata.human36m.loader import prepare_next_batch_impl as prepare_next_batch
 from hmpdata.misc.mocap_dataset import MocapDataset
+from hmpdata.human36m._dlow.euler import get_batch as DLowEulerDataLoader
 
 Human36MDataset = MocapDataset
