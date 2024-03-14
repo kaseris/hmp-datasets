2da6af453c4729f416ec0745709b3bdbcdd43de6
Michail Kaseris <mich.kaseris@gmail.com>
2024-03-14 13:03:28 +0200

    New version 1.1.0

--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -1,9 +1,12 @@
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from hmpdata.human36m._h36m import skeleton_h36m as Human36MSkeleton
 from hmpdata.human36m.loader import prepare_next_batch_impl as prepare_next_batch
 from hmpdata.misc.mocap_dataset import MocapDataset
 from hmpdata.human36m._dlow.euler import get_batch as DLowEulerDataLoader
 from hmpdata.human36m._dlow.utils import define_actions
+from hmpdata.human36m._wei import Human36M as WeiHuman36M
+from hmpdata.human36m._dlow.xyz import DatasetH36M as Human36MXYZ
+
 
 Human36MDataset = MocapDataset
