e8db5692a07fd512b35c4efbd52a87f804406dde
Michail Kaseris <mich.kaseris@gmail.com>
2024-03-21 11:35:42 +0200

    Module import

--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -7,6 +7,7 @@ from hmpdata.human36m._dlow.euler import get_batch as DLowEulerDataLoader
 from hmpdata.human36m._dlow.utils import define_actions
 from hmpdata.human36m._wei import Human36M as WeiHuman36M
 from hmpdata.human36m._dlow.xyz import DatasetH36M as Human36MXYZ
+from hmpdata.visualization import visualization as viz
 
 
 Human36MDataset = MocapDataset
