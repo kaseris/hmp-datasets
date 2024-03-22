d1cb45d638cad7439de7260f7a7755c5425a05dc
Michail Kaseris <mich.kaseris@gmail.com>
2024-03-22 16:27:50 +0200

    Minor fix. Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmpdata',
-    version='1.2.0',
+    version='1.2.1',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     # other package info
