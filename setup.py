965a26f8792087eb4841efd16564a36dd9351a28
Michail Kaseris <mich.kaseris@gmail.com>
2024-04-15 15:35:25 +0300

    New version with CLI interface for HumanEvaI

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmpdata',
-    version='1.3.4',
+    version='1.4.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     # other package info
     entry_points={
         'console_scripts': [
             'make36m=hmpdata.scripts.preprocess_h36m:main',
+            'makehumaneva=hmpdata.scripts.preprocess_humaneva:main'
         ],
     }
 )
