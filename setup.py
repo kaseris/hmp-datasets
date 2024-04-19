35ce22b9fce5fe6e242ab6c8292bbf6c12950ac2
Michail Kaseris <mich.kaseris@gmail.com>
2024-04-19 17:06:51 +0300

    New version that supports rendering the simlpe skeleton with side by side comparison

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmpdata',
-    version='1.5.1',
+    version='1.5.2',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     # other package info
--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.5.1'
+__version__ = '1.5.2'
 
 from hmpdata.human36m._h36m import skeleton_h36m as Human36MSkeleton
 from hmpdata.human36m.loader import prepare_next_batch_impl as prepare_next_batch
@@ -16,6 +16,7 @@ from hmpdata.human36m._custom import Batch as Human36MCustomBatch
 from hmpdata.human36m._simlpe.dataset_h36m import H36MDataset as Human36MsiMLPe
 from hmpdata.human36m._simlpe.config import Human36MDatasetConfig
 from hmpdata.human36m._simlpe.h36m_eval import H36MEval
+from hmpdata.human36m._simlpe.viz import render as render_siMLPe
 
 from hmpdata.humaneva.dataset_humaneva import DatasetHumanEva as HumanEva
 
