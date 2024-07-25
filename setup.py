6a53803bd596b6a82be889ccac187417e7a66ae6
Michail Kaseris <mich.kaseris@gmail.com>
2024-07-25 14:32:00 +0300

    v1.7.2: CLI support for rokoko recordings.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='hmpdata',
-    version='1.7.1',
+    version='1.7.2',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     # other package info
     entry_points={
         'console_scripts': [
             'make36m=hmpdata.scripts.preprocess_h36m:main',
-            'makehumaneva=hmpdata.scripts.preprocess_humaneva:main'
+            'makehumaneva=hmpdata.scripts.preprocess_humaneva:main',
+            'makerokoko=hmpdata.scripts.preprocess_rokoko:main'
         ],
     }
 )
