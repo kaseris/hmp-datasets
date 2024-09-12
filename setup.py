0954f8c3600fad3aaf2da2c0b8ed648091fb3aab
Michail Kaseris <mich.kaseris@gmail.com>
2024-09-12 16:48:10 +0300

    v1.9.0: Integration of the PGBIG dataset implementation of Human3.6M.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hmpdata',
-    version='1.8.1',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    name="hmpdata",
+    version="1.9.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     # other package info
     entry_points={
-        'console_scripts': [
-            'make36m=hmpdata.scripts.preprocess_h36m:main',
-            'makehumaneva=hmpdata.scripts.preprocess_humaneva:main',
-            'makerokoko=hmpdata.scripts.preprocess_rokoko:main'
+        "console_scripts": [
+            "make36m=hmpdata.scripts.preprocess_h36m:main",
+            "makehumaneva=hmpdata.scripts.preprocess_humaneva:main",
+            "makerokoko=hmpdata.scripts.preprocess_rokoko:main",
         ],
-    }
+    },
 )
--- src/hmpdata/__init__.py
+++ src/hmpdata/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.8.1'
+__version__ = "1.9.0"
 
 from hmpdata.human36m._h36m import skeleton_h36m as Human36MSkeleton
 from hmpdata.human36m.loader import prepare_next_batch_impl as prepare_next_batch
@@ -9,7 +9,11 @@ from hmpdata.human36m._wei import Human36M as WeiHuman36M
 from hmpdata.human36m._dlow.xyz import DatasetH36M as Human36MXYZ
 from hmpdata.visualization import visualization as viz
 
-from hmpdata.human36m._custom import Human36MCustomDataset, build_dataset, Human36MCustomDatasetSample
+from hmpdata.human36m._custom import (
+    Human36MCustomDataset,
+    build_dataset,
+    Human36MCustomDatasetSample,
+)
 from hmpdata.human36m._custom import Batch as Human36MCustomBatch
 
 # siMLPe
@@ -18,10 +22,15 @@ from hmpdata.human36m._simlpe.config import Human36MDatasetConfig
 from hmpdata.human36m._simlpe.h36m_eval import H36MEval
 from hmpdata.human36m._simlpe.viz import render as render_siMLPe
 
+# HumanEvaI
 from hmpdata.humaneva.dataset_humaneva import DatasetHumanEva as HumanEva
 
+# Rokoko
 from hmpdata.rokoko.dataset_rokoko import RokokoDataset as RokokoDataset
 from hmpdata.rokoko.dataset_rokoko import rokoko_collate_fn as rokoko_collate_fn
+
+# PGBIG
+from hmpdata.human36m._pgbig.dataset import Human36MPGBIGDataset as Human36MPGBIGDataset
 
 
 Human36MDataset = MocapDataset
