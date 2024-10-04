507fa3639b3223a950f10107a76e529c31650c51
Michail Kaseris <mich.kaseris@gmail.com>
2024-10-04 15:04:49 +0300

    v1.10.2: AMASS dataset returns the next pose.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hmpdata",
-    version="1.10.1",
+    version="1.10.2",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     # other package info
